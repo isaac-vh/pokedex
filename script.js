0b7c32b65ab3f1f397a2b4fdcb63f151a950edcd
isaac-vh <isaacvanhoogten@gmail.com>
2021-02-09 15:36:57 +0100

    up

--- script.js
+++ script.js
@@ -6,16 +6,37 @@ async function searchPokemon() {
         .then(res => res.json()
 
         ).then((data) => {
+
+        let pokemon = pName.toLowerCase();
+
+        if (pokemon === 'ditto') {
+            document.getElementById('move1').innerHTML = data.moves[0].move.name;
+            document.getElementById('move2').innerHTML = " ";
+            document.getElementById('move3').innerHTML = " ";
+            document.getElementById('move4').innerHTML = " ";
+
+            document.getElementById('main-screen').style.backgroundImage = `url(${data.sprites.front_default})`;
+            document.getElementById('name-screen').innerHTML = data.name;
+            document.getElementById('type-screen').innerHTML = data.types[0].type.name
+            document.getElementById('id-screen').innerHTML = data.id;
+            return
+        };
+
         console.log(data);
 
         document.getElementById('main-screen').style.backgroundImage = `url(${data.sprites.front_default})`;
         document.getElementById('name-screen').innerHTML = data.name;
-        // document.getElementById('type-screen').innerHTML = `url(${data.types.0.type.name})`
-        document.getElementById('id-screen').innerHTML = data.height;
+        document.getElementById('type-screen').innerHTML = data.types[0].type.name
+        document.getElementById('id-screen').innerHTML = data.id;
+
+        document.getElementById('move1').innerHTML = data.moves[0].move.name;
+        document.getElementById('move2').innerHTML = data.moves[1].move.name;
+        document.getElementById('move3').innerHTML = data.moves[2].move.name;
+        document.getElementById('move4').innerHTML = data.moves[3].move.name;
 
     }).catch(err => {
         alert('No pokemon found with this name!')
-        return err;
+        console.log(err)
     })
 }
 
